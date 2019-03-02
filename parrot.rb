912f2960e91b861c76ee543587c9545de1e64c34
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-02 18:52:24 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(a="Squawk")
-    puts "#{a} !"
-    returns "#{a} !"
+    puts "#{a}!"
+    returns "#{a}!"
 end
